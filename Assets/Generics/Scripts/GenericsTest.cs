2ab689b8677600569d7979eb310d96c9c942798c
CratorCreator <jordanfischer13@gmail.com>
2017-07-24 12:47:04 +1000

    V.1

---
+++ Assets/Generics/Scripts/GenericsTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Generics
+{
+    [System.Serializable]
+
+    public class GenericsTest : MonoBehaviour
+    {
+        public CustomList<GameObject> gameObjects;
+
+        // Use this for initialization
+        void Start()
+        {
+            gameObjects = new CustomList<GameObject>();
+            gameObjects[0] = new GameObject();
+            gameObjects.Add(new GameObject());
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
